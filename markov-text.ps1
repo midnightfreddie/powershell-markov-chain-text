c3a18a043e3918f4cce38638daf4c57687b1a1f7
unknown <Jim@csten.ad.xpoo.net>
2015-06-29 23:45:51 -0500

    cleanup

--- markov-text.ps1
+++ markov-text.ps1
@@ -13,27 +13,9 @@ Get-Content $env:USERPROFILE\Downloads\my-man-jeeves-project-gutenbertpg8164.txt
     ForEach-Object {
         $q.Enqueue($_)
         if ($q.Count -eq 3) {
-            #"$($q[0]) $($q[1]) $($q[2]) "
             $Key = @( $q.Dequeue(), $q.Dequeue() )
-            #$Key = @( $q.Dequeue(), $q.Dequeue() ) -join " "
             $Value = $q.Dequeue()
-            #if (-not $MarkovChain[$Key]) {
-            #    $MarkovChain.Add($Key, [string[]]$Value)
-            #} else {
-            #    $MarkovChain[$Key] += [string[]]$Value
-            #}
             $MarkovChain["$Key"] += [string[]]$Value
-            #$MarkovChain[(@( $q[0], $q[1] ))] = [string[]]$MarkovChain[(@( $q[0], $q[1] ))] + $Value
-            #$MarkovChain[(@( $q[0], $q[1] ))] = $q[2]
-            #$q[0]
-            #$q[1]
-            #$q[2]
-            #$q.Dequeue() | Out-Null
-            #"$($Key) $Value"
-            #$temp
-            #[string[]]$Value
-            #$Key
-            #$MarkovChain[$Key]
             $q.Enqueue($Key[1])
             $q.Enqueue($Value)
         }
