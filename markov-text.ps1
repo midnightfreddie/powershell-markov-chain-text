49dfaa1a7e706e3fe50d077eed099e6aabc53505
unknown <Jim@csten.ad.xpoo.net>
2015-06-29 23:48:45 -0500

    comments and moved input file name to variable

--- markov-text.ps1
+++ markov-text.ps1
@@ -2,20 +2,28 @@
 
 # Making a Markov chain text generator based on this article and Python script http://agiliq.com/blog/2009/06/generating-pseudo-random-text-with-markov-chains-u/
 
+$FilePath = "$($env:USERPROFILE)\Downloads\my-man-jeeves-project-gutenbertpg8164.txt"
+
 $MarkovChain = @{}
 $q = New-Object System.Collections.Queue
 
 # Load source text file
-Get-Content $env:USERPROFILE\Downloads\my-man-jeeves-project-gutenbertpg8164.txt |
+Get-Content $FilePath |
+    # Limit input size while developing
     Select-Object -First 1000 |
     # Since gc is one object per line, do a foreach and split the line, emitting words to pipeline
     ForEach-Object { $_.split() } |
+    # each word
     ForEach-Object {
+        # FIFO queue the word
         $q.Enqueue($_)
+        # If 3 words in the queue, create/add an entry
         if ($q.Count -eq 3) {
+            # Pop 3 items from the queue
             $Key = @( $q.Dequeue(), $q.Dequeue() )
             $Value = $q.Dequeue()
             $MarkovChain["$Key"] += [string[]]$Value
+            # Put the last two items back in the queue
             $q.Enqueue($Key[1])
             $q.Enqueue($Value)
         }
